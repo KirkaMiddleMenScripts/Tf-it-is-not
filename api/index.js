9f309153312e0555d7b8f2514cf0d93e3310360c
KirkaMiddleMenScripts <Kirkamiddlemen@gmail.com>
2025-06-02 13:35:14 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,43 +1,50 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const { user, debug } = req.query;
+  const { user } = req.query;
 
   if (!user) {
     return res.status(400).json({ error: 'Missing ?user=' });
   }
 
-  const login = user.toLowerCase(); // Twitch login name is always lowercase
+  const login = user.toLowerCase();
 
   try {
+    const query = `
+      query ChannelShell($login: String!) {
+        user(login: $login) {
+          id
+          login
+          displayName
+          stream {
+            id
+            title
+            viewersCount
+            game {
+              displayName
+            }
+          }
+        }
+      }
+    `;
+
     const gqlRes = await fetch('https://gql.twitch.tv/gql', {
       method: 'POST',
       headers: {
         'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify([
-        {
-          operationName: 'ChannelShell',
-          variables: { login },
-          extensions: {
-            persistedQuery: {
-              version: 1,
-              sha256Hash:
-                '5fa5b13fbcfbc1071e11b5b3027d05e03eac7674e7687a06b71f73e1c29c3079'
-            }
-          }
-        }
-      ])
+      body: JSON.stringify({
+        operationName: 'ChannelShell',
+        query,
+        variables: { login }
+      })
     });
 
     const json = await gqlRes.json();
+    const userData = json.data?.user;
 
-    if (debug) return res.status(200).json({ debugResponse: json });
-
-    const userData = json[0]?.data?.userOrError;
-
-    if (!userData || userData.__typename !== 'User') {
+    if (!userData) {
       return res.status(404).json({ error: 'User not found on Twitch' });
     }
 
